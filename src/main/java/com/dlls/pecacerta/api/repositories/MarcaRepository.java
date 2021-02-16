d8f55632f2aa1940da906b1a2c526bdf731d3400
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-02-15 22:57:31 -0300

    teste variaveis de ambiente

--- src/main/java/com/dlls/pecacerta/api/repositories/MarcaRepository.java
+++ src/main/java/com/dlls/pecacerta/api/repositories/MarcaRepository.java
@@ -3,8 +3,6 @@ package com.dlls.pecacerta.api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dlls.pecacerta.api.model.Categoria;
 import com.dlls.pecacerta.api.model.Marca;
 
 public interface MarcaRepository extends JpaRepository<Marca, Long>{
