f50c066e6d31e10798078125f608c759e3ff05e9
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-03-14 18:25:31 -0300

    Update ClienteRepository.java

--- src/main/java/com/dlls/pecacerta/api/repositories/ClienteRepository.java
+++ src/main/java/com/dlls/pecacerta/api/repositories/ClienteRepository.java
@@ -18,7 +18,7 @@ public interface ClienteRepository extends BaseRepository<Cliente>, JpaRepositor
 			  value = "SELECT * FROM clientes u "
 			  		+ "WHERE u.cli_nome Like ?1 OR"
 			  			+ " u.cli_cpf_cnpj Like ?1 OR"
-			  			+ " u.cli_data_nasc Like ?1 OR"
+			  			//+ " u.cli_data_nasc Like ?1 OR"
 			  			+ " u.cli_email Like ?1 OR"
 			  			+ " u.cli_telefone Like ?1",
 			  nativeQuery = true)
