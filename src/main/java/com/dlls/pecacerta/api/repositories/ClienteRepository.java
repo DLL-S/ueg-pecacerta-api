010cae8893642d8540dcf6ce78316757a52f0329
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-03-14 18:34:43 -0300

    adição de % na string

--- src/main/java/com/dlls/pecacerta/api/repositories/ClienteRepository.java
+++ src/main/java/com/dlls/pecacerta/api/repositories/ClienteRepository.java
@@ -16,11 +16,11 @@ public interface ClienteRepository extends BaseRepository<Cliente>, JpaRepositor
 	Cliente save(Cliente model);
 	@Query(
 			  value = "SELECT * FROM clientes u "
-			  		+ "WHERE u.cli_nome Like ?1 OR"
-			  			+ " u.cli_cpf_cnpj Like ?1 OR"
-			  			//+ " u.cli_data_nasc Like ?1 OR"
-			  			+ " u.cli_email Like ?1 OR"
-			  			+ " u.cli_telefone Like ?1",
+			  		+ "WHERE u.cli_nome Like %?1% OR"
+			  			+ " u.cli_cpf_cnpj Like %?1% OR"
+			  			//+ " u.cli_data_nasc Like %?1% OR"
+			  			+ " u.cli_email Like %?1% OR"
+			  			+ " u.cli_telefone Like %?1%",
 			  nativeQuery = true)
 	List<Cliente> pesquise(String termo);
 }
