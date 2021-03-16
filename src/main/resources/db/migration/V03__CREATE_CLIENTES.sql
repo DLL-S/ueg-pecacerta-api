295d4037b17ee9ecd0d92e09132f209231d94b04
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-03-16 18:55:19 -0300

    SQL

--- src/main/resources/db/migration/V03__CREATE_CLIENTES.sql
+++ src/main/resources/db/migration/V03__CREATE_CLIENTES.sql
@@ -15,4 +15,7 @@ CREATE TABLE clientes (
     cli_telefone VARCHAR(12) NOT NULL
 );
 
-INSERT INTO clientes (cli_nome,cli_tipo_cliente,cli_cpf_cnpj,cli_data_nasc,cli_logradouro,cli_numero,cli_complemento,cli_bairro,cli_cep,cli_cidade,cli_estado,cli_email,cli_telefone) VALUES ('Clio P. Wise','PESSOA_JURIDICA',54691578064,'2021-06-02','Ap #350-1754 Street',230,'624-3890 Urna St.','783-6687 Elit, St.','74.605-110','Sincelejo','Belize','netus.et.malesuada@commodoauctor.ca','40170706');
+INSERT INTO clientes (cli_nome,cli_tipo_cliente,cli_cpf_cnpj,cli_data_nasc,cli_logradouro,cli_numero,cli_complemento,cli_bairro,cli_cep,cli_cidade,cli_estado,cli_email,cli_telefone)
+VALUES ('Têm De Tudo Auto Peças S/A','PESSOA_JURIDICA',73904387000184,'2018-06-02','Rua 3, Qd 43',80,'Ao lado da drgoaria','Bom Jesus','74.605-110','Trindade','Goiás','temdetudopecas@gmail.com','3003654893');
+INSERT INTO clientes (cli_nome,cli_tipo_cliente,cli_cpf_cnpj,cli_data_nasc,cli_logradouro,cli_numero,cli_complemento,cli_bairro,cli_cep,cli_cidade,cli_estado,cli_email,cli_telefone)
+VALUES ('Damião Dos Santos e Silva','PESSOA_FISICA',23919716051,'1985-09-02','Rua 9, Lote 12',201,'Apto 206','Marista','74.485-110','Goiânia','Goiás','damiao.silva@gmail.com','62458746250');
