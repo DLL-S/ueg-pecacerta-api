676adec0637ad66ad5319cd9fdf83773255538b0
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-05-07 00:17:56 -0300

    Create EstoqueController.java

---
+++ src/main/java/com/dlls/pecacerta/api/controllers/EstoqueController.java
@@ -0,0 +1,10 @@
+package com.dlls.pecacerta.api.controllers;
+
+import com.dlls.pecacerta.api.model.MovimentacaoEstoque;
+import com.dlls.pecacerta.api.repositories.MovimentacaoEstoqueRepository;
+import com.dlls.pecacerta.api.services.EstoqueService;
+
+public class EstoqueController
+		extends BaseController<MovimentacaoEstoque, MovimentacaoEstoqueRepository, EstoqueService> {
+
+}
