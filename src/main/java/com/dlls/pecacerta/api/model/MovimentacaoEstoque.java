673369816086211ef6c04d4d1ab2650ec442a53a
leonardopereira10 <56615074+leonardopereira10@users.noreply.github.com>
2021-05-08 01:08:53 -0300

    Update MovimentacaoEstoque.java

--- src/main/java/com/dlls/pecacerta/api/model/MovimentacaoEstoque.java
+++ src/main/java/com/dlls/pecacerta/api/model/MovimentacaoEstoque.java
@@ -1,17 +1,60 @@
 package com.dlls.pecacerta.api.model;
 
+import java.util.Date;
+
+import com.dlls.pecacerta.api.utils.OperacaoEstoque;
+
 public class MovimentacaoEstoque extends BaseModel {
-
-	@Override
+	Long codigo;
+	OperacaoEstoque operacao;
+	Produto produto;
+	Double precoDeVenda;
+	Integer quantidade;
+	Date data;
+	Boolean ativo;
+	
 	public Long getCodigo() {
-		// TODO Auto-generated method stub
-		return null;
+		return codigo;
 	}
-
-	@Override
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public OperacaoEstoque getOperacao() {
+		return operacao;
+	}
+	public void setOperacao(OperacaoEstoque operacao) {
+		this.operacao = operacao;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public double getPrecoDeVenda() {
+		return precoDeVenda;
+	}
+	public void setPrecoDeVenda(double precoDeVenda) {
+		this.precoDeVenda = precoDeVenda;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
 	public void setAtivo(Boolean ativo) {
-		// TODO Auto-generated method stub
-
+		this.ativo = ativo;
 	}
+	
 
 }
